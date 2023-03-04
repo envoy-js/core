f82f56c51a36d7e2a754cea5d9520dd2c8326c0e
henry232323 <14969260+henry232323@users.noreply.github.com>
2023-03-04 14:44:47 -0700

    Generics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export default {}
+import {io} from "socket.io-client";
+
+export class Messenger<MessageType, RoomType> {
+    ws_url: string
+    room_key: keyof RoomType
+
+    constructor(ws_url: string, room_key: keyof RoomType) {
+        this.ws_url = ws_url
+        this.room_key = room_key
+    }
+}
+
+export class ChatConnection {
+    socket
+
+    constructor(ws_url: string) {
+        this.socket = io(ws_url)
+    }
+
+
+}
